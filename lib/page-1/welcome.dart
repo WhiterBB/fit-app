f52b25651b04c6e434e0836c766e7dba3608063e
Marco Salazar <57640803+WhiterBB@users.noreply.github.com>
2024-08-02 02:11:56 -0500

    Update: Remove unused imports

--- lib/page-1/welcome.dart
+++ lib/page-1/welcome.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/page-1/login.dart';
 import 'package:myapp/page-1/signup.dart';
-//import 'package:flutter/gestures.dart';
-//import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
 
 
