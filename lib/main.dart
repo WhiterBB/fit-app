94c514f5f14669505432cfc97df7cfc534db5288
Marco Salazar <57640803+WhiterBB@users.noreply.github.com>
2024-08-02 02:04:51 -0500

    Update: Remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:myapp/page-1/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/rootpage.dart';
 import 'firebase_options.dart';
-//import 'package:myapp/page-1/information.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
